d3a89949628b985b7a9ce5e91c802b8eb513d37d
Praateek Mahajan <praateekm@gmail.com>
2018-05-08 23:23:01 -0400

    Added all lol

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -37,6 +37,6 @@ X_dev = scaler.transform(X_dev)
 
 classifiers = [(GradientBoostingClassifier(), "Gradient Boosting")]
 for clf, name in classifiers:
-	clf.fit(X_train, Y_train)
+	clf.fit(X_train, y_train)
 	print("Model {0: <30} \t Score {}".format(name, clf.score(X_dev, y_dev)))
 	sys.stdout.flush()
