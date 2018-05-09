a198fbb972ed42694255fe7425a4f3c18c239d94
Praateek Mahajan <praateekm@gmail.com>
2018-05-08 23:17:49 -0400

    fit(x,y)

--- adaboost.py
+++ adaboost.py
@@ -36,6 +36,6 @@ X_dev = scaler.transform(X_dev)
 
 classifiers = [(AdaBoostClassifier(), "AdaBoost")]
 for clf, name in classifiers:
-	clf.fit(X_train)
+	clf.fit(X_train, Y_train)
 	print("Model {0: <30} \t Score {}".format(name, clf.score(X_dev, y_dev)))
 	sys.stdout.flush()
